24752299f4391838b09a05e702f14ede940624fe
Cezary Zysk <cezaryy.z97@gmail.com>
2021-05-20 16:53:32 +0200

    Created DbContext

---
+++ FoodAppApi/Models/TbUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FoodAppApi.Models
+{
+    public partial class TbUser
+    {
+        public bool? Id { get; set; }
+    }
+}
